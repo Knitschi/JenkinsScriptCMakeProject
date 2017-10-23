aea54644ab535c4350d225dc1ecedbaff5cae8c7
Knitschi <knitschi@freenet.de>
2017-10-23 21:37:29 +0200

    Initialize repository.

---
+++ JenkinsPipeline.groovy
@@ -0,0 +1,91 @@
+#!groovy
+
+/**
+This is a .groovy script for a parametrized Jenkins Pipeline Job that builds a CMake project.
+
+The job expects a jenkins node with the name 'master'. The repository will be checked out on the master
+and then copied to a slave-node that has the tag BuildSlaveTag. Then the job will call
+
+$ cmake -H. -B_build <AdditionalGenerateArguments>
+$ cmake --build _build <AdditionalBuildArguments>
+    
+in order to build the project. The job must provide the following paramters
+
+RepositoryUrl                   - The url of the git repository the contains the projects CMakeLists.txt file in the root directory.
+CheckoutDirectory               - A workspace directory on the master and build-slave into which the code is checked-out and which is used for the build.
+BuildSlaveTag                   - The tag for the build-slave on which the project is build.
+AdditionalGenerateArguments     - Arguments for the cmake generate call.
+AdditionalBuildArguments        - Arguments for the cmake build call.   
+
+*/
+
+import static Constants.*
+
+class Constants {
+    // stash names
+    static final SOURCES_STASH = "SourcesStash"
+}
+
+// This node is the driver for the subjobs
+node('master')
+{
+    addCheckoutSourcesStage()
+    addBuildStage()
+}
+
+def addCheckoutSourcesStage()
+{
+    stage('Prepare Sources')
+    {
+        ws(params.CheckoutDirectory)
+        {
+            // We do another clone/checkout because the checkout that is used to checkout this scipt ends up in a different directory. 
+            // We also want to perform a clean checkout here.
+            checkout([$class: 'GitSCM',
+                userRemoteConfigs: [[url: params.RepositoryUrl]],
+                branches: [[name: 'master']],
+                extensions: [[$class: 'CleanBeforeCheckout']]
+            )
+
+            // Stash source files for the build slaves
+            stash includes: '**', name: SOURCES_STASH
+        }
+    }
+}
+
+def addBuildStage()
+{
+    node(params.BuildSlaveTag)
+    {
+        // acquiering an extra workspace seems to be necessary to prevent interaction between
+        // the parallel run nodes, although node() should already create an own workspace.
+        ws(params.CheckoutDirectory)   
+        {   
+            // clean the workspace
+            dir(params.CheckoutDirectory)
+            {
+                deleteDir()
+            }
+            
+            // Unstash the repository content
+            unstashFiles(SOURCES_STASH, '')
+       
+            runCommand( 'cmake -H. -B_build ' + params.AdditionalGenerateArguments )
+            runCommand( 'cmake --build _build ' + params.AdditionalBuildArguments )
+            
+            echo '----- CMake project was build successfully' + toolchain + '. -----'
+        }
+    }
+}
+
+def runCommand( command )
+{
+    if(isUnix())
+    {
+        sh command
+    }
+    else
+    {
+        bat command
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CMakeProjectTestPipeline
+A Jenkins pipeline file that builds a given CMake project in various configurations.
+
+The pipeline is tailored to be used by my private Jenkins instance, so it is not of much use for other people.
